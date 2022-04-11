c253c177fbdf51af5129b9fd2f59b373d658a046
LAURA GIL <LAURA GIL@DESKTOP-P9440C4>
2022-04-11 22:26:00 +0200

    dentalClinic ifaces started

--- DentalClinic_System/src/dentalClinic/ifaces/DentistManager.java
+++ DentalClinic_System/src/dentalClinic/ifaces/DentistManager.java
@@ -1,5 +1,15 @@
 package dentalClinic.ifaces;
 
+import java.util.List;
+
+import dentalClinic.pojos.Patient;
+
+
 public interface DentistManager {
+	//Adding a new patient
+	public void addPatient (Patient p); 
+	//Search patients
+	public List<Patient> searchPatientByName(Patient p);
+	
 
 }
